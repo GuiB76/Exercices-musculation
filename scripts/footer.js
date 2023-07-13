01494025e26ea434d9cd3a748eb4705041d3b0a7
Guib76 <130552785+GuiB76@users.noreply.github.com>
2023-07-13 11:42:43 +0200

    V.0.0.4
    + page rgpd
    + changement emplacement des pages html
    +rajouts dans menuNav
    + nouvelles catégories

--- scripts/footer.js
+++ scripts/footer.js
@@ -3,15 +3,28 @@ window.addEventListener('DOMContentLoaded', function()
     // Changer les textes et liens du footer
     document.getElementById('site-name').textContent = "FaisDuSportBatard";
     document.getElementById('copyright').textContent = new Date().getFullYear();
-    document.getElementById('cgu-link').href = "/cgu.html";
-    document.getElementById('rgpd-link').href = "/rgpd.html";
+    document.getElementById('cgu-link').href = "cgu.html";
+    document.getElementById('rgpd-link').href = "rgpd.html";
 });
 
-function reloadPageOnLoad() 
-{
-    window.addEventListener("load", function() 
-    {
-        location.reload();
-    });
-}
-  
+//    function reloadPageOnLoad() 
+//    {
+//        window.addEventListener("load", function() 
+//        {
+//            location.reload();
+//        });
+//    }
+
+
+//    function reloadPageOnLoad() 
+//    {
+//        var isReloaded = false;
+//    
+//        window.addEventListener("load", function() {
+//       if (!isReloaded) {
+//            location.reload();
+//            isReloaded = true;
+//        }
+//        });
+//    }
+
